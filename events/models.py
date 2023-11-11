e448c47fc809ab8595848df9912ba7bad7f38d09
Shreya-Jayaraj <119192525+Shreya-Jayaraj@users.noreply.github.com>
2023-11-11 22:47:25 +0530

    create events

--- events/models.py
+++ events/models.py
@@ -1,10 +1,16 @@
 from django.db import models
-from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 class Event(models.Model):
     title = models.CharField(max_length = 100)
     content = models.TextField()
-    date = models.DateTimeField(default=timezone.now())
+    date = models.DateField()
     organization = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+    
+    def get_absolute_url(self):
+        return reverse('event-detail', kwargs={'pk':self.pk})
 
--- events/urls.py
+++ events/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
+from .views import EventListView, EventDetailView, EventCreateView
 
 urlpatterns = [
-    path('', views.home, name='events-home'),
+    path('', EventListView.as_view() , name='events-home'),
+    path('event/<int:pk>/', EventDetailView.as_view() , name='event-detail'),
+    path('event/new/', EventCreateView.as_view() , name='event-create'),
     path('about/', views.about, name='events-about'),
 ]
