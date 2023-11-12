4af2d620b19c4fa83575becc9847fefdde3a9738
Devika rajeevan <devikarajeevan2004@gmail.com>
2023-11-12 18:08:26 +0530

    calender added

--- events/models.py
+++ events/models.py
@@ -6,6 +6,7 @@ class Event(models.Model):
     title = models.CharField(max_length = 100)
     content = models.TextField()
     date = models.DateField()
+    
     organization = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
